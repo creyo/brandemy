9403560de65e9b7707862d0db40933e2fa8cd28d
Ali Raza <alirazaqureshi43@gmail.com>
2023-11-08 14:50:58 +0500

    home

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,16 @@
 <script>
     import logo from '../lib/images/logo.png';
+    import { page } from '$app/stores';
+    /**
+   * @type {boolean}
+   */
+    let home;
+    $page.url.pathname === '/' ? home = true : home = false;
+
 </script>
+{#if !home}
+
 <div class="navbar">
     <img src={logo} alt="">
 </div>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,13 @@
 	import './styles.css';
 	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
+
 </script>
 
 <div class="app">
+ 
 	<Header/>
+ 
 	<main>
 		<slot />
 	</main>
