a4b73ed4d6de38aacd077da42ebe811167f2b229
Ali Raza <alirazaqureshi43@gmail.com>
2023-11-27 18:06:31 +0500

    img-size

--- src/components/blogCard.svelte
+++ src/components/blogCard.svelte
@@ -14,7 +14,7 @@
         <p>Branding Strategies</p>
         <p>{blog.authors.name}</p>
     </div>
-    <img src={`https://res.cloudinary.com/creyo-com/image/upload/v1698497773/brandemy/blog/${blog.featured_image}`} alt="">
+    <img src={`https://res.cloudinary.com/creyo-com/image/upload/c_scale,w_375,h_215/v1698497773/brandemy/blog/${blog.featured_image}`} alt="">
     <h2>{blog.title}</h2>
     <p>{blog.seo_description}</p>
 </div>
